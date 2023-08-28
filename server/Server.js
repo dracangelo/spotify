b4e08075fc19c49b7589672b85cfd41d5052da53
dracangelo <kelvinmbuguaw@gmail.com>
2023-08-28 11:22:20 +0300

    added a dashboard.js. worked on server.js

---
+++ server/Server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+// login application
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    // new spotify web api
+    const spotifyApi = new SpotifyWebApi ({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'c85bb889d5674e5aae41a3b5d9efa39a',
+        clientSecret: '85239db1bec54f02b357dd39fa4e7c9f'
+        
+    })
+    spotifyApi.authorizationCodeGrant(code).then(date => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+    })
+    // error response fallback
+    .catch(() => {
+        res.sendStatus(400) 
+    })
+});
